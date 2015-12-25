46842cf3b01bbfde93d170bb15bd64411f49b2c1
davidpobi <pobidavid@gmail.com>
2015-12-25 21:45:14 +0000

    UX

--- js/noticesApp.js
+++ js/noticesApp.js
@@ -3,11 +3,14 @@ var app = angular.module('noticesApp', []);
 app.controller('mainController', function($scope){
 	$scope.notices = [];
 	$scope.newNotice = {created_by: '', text: '', created_at: ''};
+	
 
 	$scope.post = function(){
 		$scope.newNotice.created_at = Date.now();
 		$scope.notices.push($scope.newNotice);
 		$scope.newNotice = {created_by: '', text: '', created_at: ''};
+		// $('#newIcon').css('animation','newEntry');
+
 	};
 });
 
