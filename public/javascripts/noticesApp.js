e431e45c990160688b93bdab7180cd91e804c0ba
abdulmuhsinGH <abdul.iddris13@gmail.com>
2016-01-28 12:09:39 +0000

    incomplete -- login authentication

--- public/javascripts/noticesApp.js
+++ public/javascripts/noticesApp.js
@@ -1,5 +1,11 @@
 var noticesApp = angular.module('noticesApp', ['ngRoute']);
 console.log('inside notices app');
+
+noticesApp.run(function($rootScope){
+   $rootScope.authenticated = false;
+   $rootScope.current_user = '';
+   console.log($rootScope.current_user);
+});
 noticesApp.config(function($routeProvider){
   $routeProvider
     //the timeline display
@@ -33,13 +39,25 @@ noticesApp.controller('mainController',['$scope', function($scope){
 	};
 }]);
 
-noticesApp.controller('authController',['$scope', function($scope){
+noticesApp.controller('authController',['$scope', function($scope,$http,$rootScope,$location){
 	$scope.user = {username: '', password: ''};
 	$scope.error_message = '';
 
 	$scope.login = function(){
     	//placeholder until authentication is implemented
-    	$scope.error_message = 'login request for ' + $scope.user.username;
+      
+      $http.post('/auth/login', $scope.user).success(function(data){
+        console.log('hello')
+        if(data.state == 'success'){
+          $rootScope.authenticated = true;
+          $rootScope.current_user = data.user.username;
+          location.path('/');
+        }
+        else {
+          $scope.error_message = 'invalid login details';
+        }
+      });
+    	
 	};
 
 	$scope.register = function(){
--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,6 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var LocalStrategy = require('passport-local').Strategy;
+var bCrypt = require('bCrypt');
 
+var users = {};
+console.log("auth");
+
+passport.use('login', new LocalStrategy({
+    passReqToCallback : true
+},
+
+function(req, username,password,done){
+    if (users[username]) {
+        console.log('User not found with username'+ username);
+        return done(null,false);
+    }
+
+    if (isValidPassword(users[username], password)) {
+        //sucessfully authenticated
+        return done(null,users[username]);
+
+    } 
+    else{
+        console.log('Invalid password'+username);
+        return done(null,false);
+    }
+
+    }
+
+
+
+));
+
+
+User.findOne({'username' : username },
+    function(err, user){
+        //In case of any error, return using the done method 
+        if(err)
+            return done(err);
+        //username does not exist, log error and redirect back
+        if(!user){
+            console.log('User not found with username'+username);
+            return done(null, false);
+        }
+        //user exists but wron password, log the error
+        if (!isValidPassword(user,password)) {
+            console.log('Invalid Password');
+            return done(null,false); //redirect back to login page
+        }
+
+        //User and password both match, return user from done method
+        //which will be treated  like success
+        return done(null,user);
+    })
 module.exports = function(passport){
 
     //sends successful login state back to angular
