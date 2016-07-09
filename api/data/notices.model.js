899762a09563d956da2923ca1c689ad0e027e42e
abdulmuhsinGH <abdul.iddris13@gmail.com>
2016-07-09 17:44:02 +0000

    --restructuring db

--- api/data/notices.model.js
+++ api/data/notices.model.js
@@ -4,16 +4,58 @@ var mongoose = require('mongoose'),
     bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
     username: String,
     password: String, //hash created from password
-    created_at: {type: Date, default: Date.now}
+    privilege: String,
+    organisations:[OrganisationSchema],
+    is_deleted: { type: Number, 'default': 0 },
+    created_at: {type: Date, 'default': Date.now}
 });
 
 var PostSchema = new mongoose.Schema({
     created_by: {type: mongoose.Schema.ObjectId, ref: 'User'},
-    created_at: {type: Date, default: Date.now},
-    text: String
+    created_at: {type: Date, 'default': Date.now},
+    text: String,
+    title: String,
+    category: String,
+    is_deleted: { type: Number, 'default': 0 },
+    location:{ type:[Number], index:'2dsphere'},
+    resources:[ResourceSchema],
+    image:[ImageSchema],
+    links:[String],
+    video:[VideoSchema]
 });
+
+var ResourceSchema = new mongoose.Schema({
+  file_name: String,
+  file_type: String,
+  created_at: {type: Date, 'default': Date.now}
+});
+
+var ImageSchema = new mongoose.Schema({
+  image_name: String,
+  image_type: String,
+  created_at: {type: Date, 'default': Date.now}
+});
+
+var VideoSchema = new mongoose.Schema({
+  video_name: String,
+  video_type: String,
+  created_at: {type: Date, 'default': Date.now}
+});
+
+var OrganisationSchema = new mongoose.Schema({
+  name: String,
+  description:String
+  created_at: {type: Date, 'default': Date.now}
+})
+
+
+
+
 
 UserSchema.plugin(passportLocalMongoose);
 
