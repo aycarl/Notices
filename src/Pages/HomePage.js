b1030a011a8e5095af98194ff465e45fa9ed74c1
Carl <carl.agbenyega@gmail.com>
2020-05-03 15:53:53 -0600

    edited App.js adn added page components

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class HomePage extends Component{
+  constructor(props){
+    super();
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+          Notice
+        </header>
+      </div>
+    )
+  }
+}
+
+export default HomePage
