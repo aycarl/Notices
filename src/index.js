f731f5594085a064bcf1a5cec35a12afa56b7cbd
Carl <carl.agbenyega@gmail.com>
2020-06-23 23:37:50 -0600

    added and configured redux-persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-
-import store from './redux/store'
+import { PersistGate } from "redux-persist/integration/react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
       <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer'
 import noticeReducer from './notices/notice.reducer'
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['noticeBoard']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   noticeBoard: noticeReducer
 });
+
+export default persistReducer(persistConfig, rootReducer)
