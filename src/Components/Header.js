61c2a09f4b4b77d16da7985cf35a07e865f2137b
Carl <carl.agbenyega@gmail.com>
2020-06-30 23:16:11 -0600

    updated header styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import Nav from "react-bootstrap/Nav";
+import { Nav, Navbar } from "react-bootstrap";
 
 import { auth } from "../firebase/firebase.utils";
 
@@ -9,45 +9,48 @@ import styles from "./../Assets/stylesheets/header.module.scss";
 
 const Header = ({ currentUser }) => {
   return (
-    <Nav className={`${styles.header} justify-content-end`} activeKey="/">
-      <Nav.Item className={styles.options}>
-        <Nav.Link href="/">Home</Nav.Link>
-      </Nav.Item>
-      {currentUser ? (
+    <Navbar bg="light">
+      <Nav className={`${styles.header} justify-content-end`} activeKey="/">
+        <Navbar.Brand className={styles.brandName} href="/">React-Bootstrap</Navbar.Brand>
         <Nav.Item className={styles.options}>
-          <Nav.Link
-            href={`/${currentUser.displayName}`}
-            eventKey="link-profile"
-          >
-            Your profile
-          </Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
         </Nav.Item>
-      ) : (
-        <Nav.Item className={styles.options}>
-          <Nav.Link href="/about" eventKey="link-about">
-            About
-          </Nav.Link>
-        </Nav.Item>
-      )}
-      {currentUser ? (
-        <Nav.Item className={styles.options}>
-          <Nav.Link onClick={() => auth.signOut()} eventKey="link-sign-out">
-            Sign Out
-          </Nav.Link>
-        </Nav.Item>
-      ) : (
-        <Nav.Item className={styles.options}>
-          <Nav.Link href="/signin" eventKey="link-sign-in">
-            Sign In
-          </Nav.Link>
-        </Nav.Item>
-      )}
-    </Nav>
+        {currentUser ? (
+          <Nav.Item className={styles.options}>
+            <Nav.Link
+              href={`/${currentUser.displayName}`}
+              eventKey="link-profile"
+            >
+              Profile
+            </Nav.Link>
+          </Nav.Item>
+        ) : (
+          <Nav.Item className={styles.options}>
+            <Nav.Link href="/about" eventKey="link-about">
+              About
+            </Nav.Link>
+          </Nav.Item>
+        )}
+        {currentUser ? (
+          <Nav.Item className={styles.options}>
+            <Nav.Link onClick={() => auth.signOut()} eventKey="link-sign-out">
+              Sign Out
+            </Nav.Link>
+          </Nav.Item>
+        ) : (
+          <Nav.Item className={styles.options}>
+            <Nav.Link href="/signin" eventKey="link-sign-in">
+              Sign In
+            </Nav.Link>
+          </Nav.Item>
+        )}
+      </Nav>
+    </Navbar>
   );
 };
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
 
 export default connect(mapStateToProps)(Header);
