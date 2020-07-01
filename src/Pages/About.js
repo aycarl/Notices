fcf785dfed79936962ce30634e02afaccbc68360
Carl <carl.agbenyega@gmail.com>
2020-06-30 23:16:15 -0600

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+import "./../Assets/stylesheets/pages.modules.scss";
 
 const About = () => {
   return (
-    <div className="App">
-      <header className="App-header">About</header>
-    </div>
+    <Container fluid className="page">
+      <h1>About</h1>
+      <hr />
+      <p>This is a simple virtual notice board application.</p>
+      <p>Feel free to catch up with random posts and add yours too!</p>
+    </Container>
   );
 };
 
