ac8ebcd6bb32dd3763d496c5e01b22879bd4144d
Carl <carl.agbenyega@gmail.com>
2020-07-03 22:28:58 -0600

    Updates

---
+++ precache-manifest.47c9c5cf9721e146794cccef7fff0992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d3730c69f472dbb53e1235a49eaeed",
+    "url": "/notices/index.html"
+  },
+  {
+    "revision": "ea982e10a17669c539f0",
+    "url": "/notices/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4a73c7debd17eec08188",
+    "url": "/notices/static/css/main.13d228ad.chunk.css"
+  },
+  {
+    "revision": "ea982e10a17669c539f0",
+    "url": "/notices/static/js/2.72ca25b6.chunk.js"
+  },
+  {
+    "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
+    "url": "/notices/static/js/2.72ca25b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a73c7debd17eec08188",
+    "url": "/notices/static/js/main.b9cb4c8b.chunk.js"
+  },
+  {
+    "revision": "a63d9f448350c5438551",
+    "url": "/notices/static/js/runtime-main.4853a2ae.js"
+  }
+]);
